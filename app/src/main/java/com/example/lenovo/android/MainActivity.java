0677cc4f2373b2b80ffc30732702dc9b1a71a758
ybt <755855627@qq.com>
2019-03-18 19:06:52 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lenovo/android/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.lenovo.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.linearlayout);
+        //setContentView(R.layout.constraintlayout);
+        setContentView(R.layout.tablelayout);
+    }
+}
